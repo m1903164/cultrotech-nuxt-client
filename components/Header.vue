8dc73ac6ceae98d9fd41264f96d9ab9a4ff8b081
m1903164 <johnfrontend001@gmail.com>
2025-03-08 21:10:09 +0300

    add scss & test it

--- components/Header.vue
+++ components/Header.vue
@@ -1,5 +1,32 @@
 <template>
-    <div>
-        Header
-    </div>
+    <header class="header">
+        <div class="header__container">
+            <nav class="header__menu">
+                <ul class="header__menu__list">
+                    <li>О комании</li>
+                    <li>Новости</li>
+                    <li>Галерея</li>
+                    <li>Доставка и опалата</li>
+                    <li>Контакты</li>
+                    <li>Парнеры</li>
+                </ul>
+            </nav>
+        </div>
+    </header>
 </template>
+
+<style scoped lang="scss">
+.header {
+    &__container {
+        max-width: 1170px;
+    }
+
+    &__menu {
+        &__list {
+            display: flex;
+            list-style: none;
+            gap: 40px;
+        }
+    }
+}
+</style>
